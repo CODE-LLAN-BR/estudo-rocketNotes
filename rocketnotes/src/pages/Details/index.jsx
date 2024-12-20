51e62a0aafab9113aac644433ee007971a5da93f
Leonardo Nunes <leonardonunes1314@gmail.com>
2024-12-20 11:37:01 -0300

    CSS in JS, install Styled components

--- rocketnotes/src/pages/Details/index.jsx
+++ rocketnotes/src/pages/Details/index.jsx
@@ -1,9 +1,13 @@
-import "./style.css"
+import { Container } from "./style";
 
 export function Details() {
 
   return (
+    <Container>
     <h1>Hello World!</h1>
+    <h2>LLAN</h2>
+    </Container>
+    
 
   )
 }
---
+++ rocketnotes/src/pages/Details/style.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+
+export const Container = styled.div`
+    margin:0px;
+    padding:0px;
+    
+    
+    background-color: red;
+
+    color:white;
+
+    h1{
+    color:black;
+    font-size:42px;
+    }
+
+`;
+
+
+
+
+
