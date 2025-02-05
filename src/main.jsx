3e3a3498587d3f0445acdbe42ad6f6eb8e974ce5
Leonardo Nunes <leonardonunes1314@gmail.com>
2025-02-05 14:16:47 -0300

    add archive myContext and config myContext provider

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+
+import theme from './pages/Styles/theme';
+import GlobalStyles from './pages/Styles/global';
+import { Routes } from './routes';
+import { MyContext } from './myContext';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles/>
+        <MyContext.Provider value = {{name:"leonardo"}}>
+          <Routes />
+        </MyContext.Provider>
+    </ThemeProvider>
+  </React.StrictMode>,
+);
+
